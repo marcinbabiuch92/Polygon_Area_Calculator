38c62ea999ff33d580626808c9bac2147b0ca35a
piroman <marcin.babiuch92@gmail.com>
2021-11-21 00:37:33 +0100

    Added rectangle class and few methods

--- main.py
+++ main.py
@@ -8,13 +8,14 @@ print(rect.get_area())
 rect.set_width(3)
 print(rect.get_perimeter())
 print(rect)
+print(rect.get_picture())
 
-sq = shape_calculator.Square(9)
-print(sq.get_area())
-sq.set_side(4)
-print(sq.get_diagonal())
-print(sq)
+# sq = shape_calculator.Square(9)
+# print(sq.get_area())
+# sq.set_side(4)
+# print(sq.get_diagonal())
+# print(sq)
 
 
 # Run unit tests automatically
-main(module='test_module', exit=False)
+# main(module='test_module', exit=False)
--- shape_calculator.py
+++ shape_calculator.py
@@ -1,6 +1,36 @@
 class Rectangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def __repr__(self):
+        return f"Rectangle(width={self.width}, height={self.height})"
+
+    def set_width(self, width):
+        self.width = width
+
+    def set_height(self, height):
+        self.height = height
+
+    def get_area(self):
+        return self.width * self.height
+
+    def get_perimeter(self):
+        return (2 * self.width + 2 * self.height)
+
+    def get_diagonal(self):
+        return ((self.width ** 2 + self.height ** 2) ** .5)
+
+    def get_picture(self):
+        picture = str()
+        if self.width > 50 or self.height > 50:
+            return "Too big for picture"
+        for y in range(self.height):
+            for x in range(self.width):
+                picture += '*'
+            picture += '\n'
+        return picture
 
 
 
-
-class Square:
+# class Square:
